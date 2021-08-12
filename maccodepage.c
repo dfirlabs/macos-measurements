c878e82977a564c6d662253117dd47aa1dbb1f98
Joachim Metz <joachim.metz@gmail.com>
2021-08-12 19:38:31 +0200

    Worked on tool to export codepage information

--- maccodepage.c
+++ maccodepage.c
@@ -1,5 +1,5 @@
 /*
- * CLI tool to export Unicode to codepage mappings using CoreFoundation.
+ * CLI tool to export codepage to Unicode mappings using CoreFoundation.
  *
  * Copyright (C) 2021, Joachim Metz <joachim.metz@gmail.com>
  *
@@ -134,6 +134,10 @@ void cfstring_printf(
          formatted_string );
 }
 
+#if defined( MACCODEPAGE_UNICODE_TO_CODEPAGE )
+
+/* TODO move this into function */
+
 int main(
      int argc,
      char * const argv[] )
@@ -141,11 +145,11 @@ int main(
 	UniChar utf16_string[ 2 ]        = { 0, 0 };
 	CFDataRef encoded_string         = NULL;
 	CFStringRef unicode_string       = NULL;
-	const UniChar *chars             = NULL;
 	const UInt8 *encoded_string_data = NULL;
 	maccodepage_encoding_t *encoding = NULL;
 	CFIndex encoded_string_length    = 0;
 	uint32_t unicode_character       = 0;
+	int empty_line                   = 0;
 	int encoding_index               = 0;
 
 	if( argc != 2 )
@@ -174,8 +178,10 @@ int main(
 	 CFSTR("Codepage: %s\n"),
 	 encoding->name );
 
+	/* TODO add support for > 0x00010000UL < 0x0010ffffUL */
+
 	for( unicode_character = 0x00000000UL;
-	     unicode_character < 0x0010ffffUL;
+	     unicode_character < 0x00010000UL;
 	     unicode_character++ )
 	{
 		utf16_string[ 0 ] = (UniChar) unicode_character;
@@ -220,16 +226,142 @@ int main(
 					else if( encoded_string_length != 0 )
 					{
 						cfstring_printf(
-						 CFSTR("0x%08" PRIx32 " <UNSUPPORTED: %d>\n"),
-						 unicode_character,
+						 CFSTR(" <UNSUPPORTED: %d>\n"),
 						 (int) encoded_string_length );
 					}
+					empty_line = 0;
 				}
-				cfstring_printf(
-				 CFSTR("\n") );
-
+				if( empty_line == 0 )
+				{
+					cfstring_printf(
+					 CFSTR("\n") );
+				}
 				CFRelease(
 				 encoded_string );
+
+				if( encoded_string_length == 0 )
+				{
+					empty_line = 1;
+				}
+			}
+			CFRelease(
+			 unicode_string );
+		}
+	}
+	return( EXIT_SUCCESS );
+}
+
+#endif /* defined( MACCODEPAGE_UNICODE_TO_CODEPAGE ) */
+
+int main(
+     int argc,
+     char * const argv[] )
+{
+	UInt8 byte_string[ 16 ]          = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	CFDataRef encoded_string         = NULL;
+	CFStringRef unicode_string       = NULL;
+	maccodepage_encoding_t *encoding = NULL;
+	CFIndex unicode_string_index     = 0;
+	CFIndex unicode_string_length    = 0;
+	UniChar unicode_character        = 0;
+	uint32_t codepage_character      = 0;
+	uint32_t codepage_character_copy = 0;
+	int byte_string_index            = 0;
+	int empty_line                   = 0;
+	int encoding_index               = 0;
+
+	if( argc != 2 )
+	{
+		fprintf(
+		 stderr,
+		 "Missing encoding index.\n" );
+
+		return( EXIT_FAILURE );
+	}
+	encoding_index = atoi( argv[ 1 ] );
+
+	if( ( encoding_index < 0 )
+	 || ( encoding_index >= 42 ) )
+	{
+		fprintf(
+		 stderr,
+		 "Unsupported encoding index: %d.\n",
+		 encoding_index );
+
+		return( EXIT_FAILURE );
+	}
+	encoding = &( maccodepage_encodings[ encoding_index ] );
+
+	cfstring_printf(
+	 CFSTR("Codepage: %s\n"),
+	 encoding->name );
+
+	for( codepage_character = 0x00000000UL;
+	     codepage_character < 0x00010000UL;
+	     codepage_character++ )
+	{
+		byte_string_index       = 0;
+		codepage_character_copy = codepage_character;
+
+		do
+		{
+			byte_string[ byte_string_index++ ] = (UInt8) codepage_character_copy & 0xff;
+
+			codepage_character_copy >>= 8;
+		}
+		while( codepage_character_copy > 0 );
+
+		byte_string[ byte_string_index++ ] = 0;
+
+		unicode_string = CFStringCreateWithBytes(
+				  NULL,
+				  byte_string,
+				  byte_string_index - 1,
+		                  encoding->codepage,
+				  0 );
+
+		if( unicode_string != NULL )
+		{
+			unicode_string_length = CFStringGetLength(
+						 unicode_string );
+
+			if( unicode_string_length != 0 )
+			{
+				cfstring_printf(
+				 CFSTR("0x%04" PRIx32 " =>"),
+				 codepage_character );
+
+				if( unicode_string_length <= 6 )
+				{
+					for( unicode_string_index = 0;
+					     unicode_string_index < unicode_string_length;
+					     unicode_string_index++ )
+					{
+						unicode_character = CFStringGetCharacterAtIndex(
+						                     unicode_string,
+						                     unicode_string_index );
+
+						cfstring_printf(
+						 CFSTR(" 0x%04" PRIx32 ""),
+						 unicode_character );
+					}
+				}
+				else if( unicode_string_length != 0 )
+				{
+					cfstring_printf(
+					 CFSTR(" <UNSUPPORTED: %d>\n"),
+					 (int) unicode_string_length );
+				}
+				empty_line = 0;
+			}
+			if( empty_line == 0 )
+			{
+				cfstring_printf(
+				 CFSTR("\n") );
+			}
+			if( unicode_string_length == 0 )
+			{
+				empty_line = 1;
 			}
 			CFRelease(
 			 unicode_string );
